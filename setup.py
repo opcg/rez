259b9ded65b2bb0d1d49deed4000cdee58ac833a
marcus <konstruktion@gmail.com>
2019-05-30 09:09:35 +0100

    Add Python 3 to setup.py

--- setup.py
+++ setup.py
@@ -120,6 +120,9 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development",
         "Topic :: System :: Software Distribution"
     ]
